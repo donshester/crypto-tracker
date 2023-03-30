1d4fbb8069ec5ff8825cba3134ea0fe38fd2b0e6
Vladislav Stepanov <vlad.stepanov.2003@bk.ru>
2023-03-31 02:23:21 +0300

    feat: added dynamic info on header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,43 +1,33 @@
 import React from 'react'
 import './Header.scss'
 import { Portfolio } from '../Portfolio/Portfolio'
+import { useSelector} from "react-redux";
+import {ICoinDataState} from '../../redux/reducers/cryptosReducer';
 
-interface ICrypto {
-  name: string
-  symbol: string
-  price: number
-}
-
-interface IPortfolio {
-  currentPrice: number
-  absoluteDifference: number
-  relativeDifference: number
-}
 
 const Header: React.FC = () => {
-  const cryptos: ICrypto[] = [
-    { name: 'Bitcoin', symbol: 'BTC', price: 20329.01 },
-    { name: 'Ethereum', symbol: 'ETH', price: 1456.21 },
-    { name: 'Binance Coin', symbol: 'BNB', price: 274.13 },
-  ]
+    const { data, isLoading, error } = useSelector(
+        (state: { cryptos: ICoinDataState }) => state.cryptos,
+    )
+    const topCryptos = data.slice(0,3)
 
-  return (
-    <header className='Header'>
-      <div className='Header__logo'></div>
-      <h1 className='Header__title'>Crypto Tracker</h1>
-      <div className='Header__cryptos'>
-        {cryptos.map((crypto) => (
-          <div className='Header__crypto' key={crypto.symbol}>
-            <div className='Header__cryptoSymbol'>{crypto.symbol}</div>
-            <div className='Header__cryptoName'>{crypto.name}</div>
-            <div className='Header__cryptoPrice'>{crypto.price}</div>
-          </div>
-        ))}
-      </div>
-      <div className={'Header__portfolio'}>
-        <Portfolio />
-      </div>
-    </header>
+    return (
+        <header className='Header'>
+            <div className='Header__logo'></div>
+            <h1 className='Header__title'>Crypto Tracker</h1>
+            <div className='Header__cryptos'>
+                {topCryptos.map((crypto) => (
+                    <div className='Header__crypto' key={crypto.id}>
+                        <div className='Header__cryptoSymbol'>{crypto.name}</div>
+                        <div className='Header__cryptoName'>{crypto.name}</div>
+                        <div className='Header__cryptoPrice'>{'$'+ Number(crypto.price).toFixed(2)}</div>
+                    </div>
+                ))}
+            </div>
+            <div className={'Header__portfolio'}>
+                <Portfolio />
+            </div>
+        </header>
   )
 }
 
--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -1,24 +1,44 @@
-import React, {useState} from 'react'
-
+import React, {useEffect, useState} from 'react'
 import './Portfolio.scss'
-import {useSelector} from 'react-redux';
-import {AppState} from '../../redux/store';
+import {useDispatch, useSelector} from 'react-redux';
+import {AppDispatch, AppState} from '../../redux/store';
 import PortfolioModal from '../PortfolioModal/PortfolioModal';
+import {fetchCryptoInfo, ICryptoInfoResponse} from '../../redux/thunks/fetchCryptoInfoThunk';
+import {correctCryptoParam} from '../helpers';
 
 export const Portfolio: React.FC = () => {
+  const dispatch:AppDispatch = useDispatch();
   const [isModalOpen, setIsModalOpen] = useState(false)
-  const balance = useSelector((state: AppState) => state.portfolio.balance);
+  const portfolio = useSelector((state: AppState) => state.portfolio);
+  const [portfolioPrice, setPortfolioPrice] = useState<number>(0);
+  const initBalance = 100;
+  useEffect(() => {
+    const fetchPortfolioPrice = async () => {
+      let totalPrice = 0;
+      for (const currency of portfolio.currencies) {
+        const cryptoInfo = await dispatch(fetchCryptoInfo(correctCryptoParam(currency.name)));
+        if (cryptoInfo && cryptoInfo.priceUsd) {
+          totalPrice += parseFloat(cryptoInfo.priceUsd ) * currency.quantity;
+        }
+      }
+      setPortfolioPrice(totalPrice);
+    };
+    fetchPortfolioPrice();
+  }, [dispatch, portfolio]);
 
-
+  const absolutePrice: number = portfolioPrice + portfolio.balance;
+  const absoluteDifference: number = absolutePrice - initBalance;
+  const percentDifference: number = (absolutePrice/initBalance - 1)*100;
   const handleCloseModal = () => {
     setIsModalOpen(false)
   }
 
     return (
     <div className='Portfolio'>
-      <div className='Portfolio__balance'> {`Balance:${balance}  USD`}</div>
-      <div className='Portfolio__absolute-change'>Change: +2.38</div>
-      <div className='Portfolio__percent-change'> (+34.55%)</div>
+      <div className='Portfolio__balance'> {`Free Balance:$${portfolio.balance.toFixed(2)}`}</div>
+      <div className='Portfolio__portfolio-price'> {`Portfolio:$${absolutePrice.toFixed(2)}`}</div>
+      <div className='Portfolio__absolute-change'>{`Change: $${absoluteDifference.toFixed(2)}`}</div>
+      <div className={percentDifference > 0 ?'Portfolio__percent-change--positive':'Portfolio__percent-change--negative' }> {`(${percentDifference.toFixed(2)}%)`}</div>
       <button onClick={() => setIsModalOpen(true)}>Details:</button>
       {isModalOpen && <PortfolioModal isOpen={isModalOpen} onClose={handleCloseModal} />}
     </div>
